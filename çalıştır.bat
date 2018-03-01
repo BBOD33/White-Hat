d6f6e325499e462400d538ba0f7c8cdae4fcd794
BBOD33 <33727551+BBOD33@users.noreply.github.com>
2018-03-01 21:08:16 +0200

    Delete çalıştır.bat

--- çalıştır.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-echo Bu skript sizi hatalardan ve kapanmalardan kurtarýr!
-title Bot AnaPanel
-:StartServer
-node bot.js
-echo (%time%) Bot hata verdi veya kapandý, yeniden baþlýyor...
-goto StartServer
